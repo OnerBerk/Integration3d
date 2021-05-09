e84c1ae65ad42737c02cd49bfd3f58e062f6101b
OnerBerk <63228692+OnerBerk@users.noreply.github.com>
2021-05-09 17:55:20 +0200

    nouvelle page d'integration complete

--- canvas_back/src/index.js
+++ canvas_back/src/index.js
@@ -11,7 +11,7 @@ const indent = require("./utils/beautify-indent");
 
 app.use(cors());
 
-downloadFunc.downloadZip(rgx.titacZipUrl);
+//downloadFunc.downloadZip(rgx.titacZipUrl);
 
 setTimeout(() => {
     indent.indent(rgx.configPath)
---
+++ canvas_front/config-overrides.js
@@ -0,0 +1,12 @@
+module.exports = function override(config, env) {
+    const path = require('path')
+
+    module.exports = {
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        }
+    }
+    return config;
+}
