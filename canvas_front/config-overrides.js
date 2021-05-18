2eb87e1aba06bfec8d9d6c9a0bded7d78b92cddf
OnerBerk <63228692+OnerBerk@users.noreply.github.com>
2021-05-18 14:10:48 +0200

    keep function filename & custom integration

--- canvas_front/config-overrides.js
+++ canvas_front/config-overrides.js
@@ -1,15 +1,24 @@
-const paths = require('paths');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserMinimizer = require('terser-webpack-plugin');
 
 module.exports = function override(config, env) {
     config.output.filename = 'static/js/bundle.js'
     config.optimization={
-        splitChunks: {
-            cacheGroups: {default: false}
-        },
+        splitChunks: {cacheGroups: {default: false}},
         runtimeChunk: false,
+        minimizer:[
+            new TerserMinimizer({
+                terserOptions: {
+                    mangle: {
+                        keep_classnames: true,
+                        keep_fnames: true,
+                    }
+                }
+            })
+        ],
     }
-    config.plugins.filename = 'static/css/bundle.css'
-    config.plugins.chunkFilename = 'static/css/bundle.css'
+    config.plugins.push(new MiniCssExtractPlugin({
+        filename: 'static/css/main.css',
+    }))
     return config
 }
